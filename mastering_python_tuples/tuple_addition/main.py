4735596d30a781b2db00ee39b7e6041c5359041b
BN-Master <>
2025-12-30 00:17:32 +0000

    Tuple addition

---
+++ mastering_python_tuples/tuple_addition/main.py
@@ -0,0 +1,7 @@
+animal_movies = ('The Lion King', 'Jurassic Park', 'Finding Nemo')
+
+# Write your code here
+new_movie = ('Dumbo', 'Zootopia',)
+animal_movies += new_movie
+
+print("Updated animal movies:", animal_movies)
